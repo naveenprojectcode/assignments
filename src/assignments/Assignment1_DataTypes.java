734295056f1c5e34ed20f1f5d4ef99ffcd5d8af8
Naveen_Prasath <naveenmailid95@gmail.com>
2025-08-08 12:22:26 +0530

    Assignment1_completed

---
+++ src/assignments/Assignment1_DataTypes.java
@@ -0,0 +1,119 @@
+package assignments;
+/** Daily Assignments
+ 
+ */
+
+
+public class Assignment1_DataTypes {
+
+	public static void main(String[] args) {
+		//Data type assignments
+		
+		System.out.println("Data Type Assignments shared below :");
+		
+		//Temperature of a city in degrees Celsius: 25.5
+		float temperature=25.5f;
+		System.out.println("Temperature of a city in degrees Celsius:"+temperature);
+		
+		//Whether a customer has placed an order: true or false
+		
+		boolean customer=true;
+		
+		System.out.println("Whether a customer has placed an order: true or false "+customer);
+		
+		//Person's phone number: "123-456-7890"
+		 
+		long phone_number=1234567890l;
+		
+		System.out.println("Person's phone number: "+phone_number);
+		
+		// Amount of money in a customer's bank account: 1000.50
+		float money=100.50f;
+		
+		System.out.println("Amount of money in a customer's bank account:"+money);
+		
+		// Person's email address: "john.doe@example.com"
+		
+		String email="john.doe@example.com";
+		System.out.println("Person's email address: "+email);
+		
+		
+		// Coordinates of a location (latitude, longitude): 37.7749, -122.4194
+		
+		float latitude=37.7749f;
+		float longitude=-122.4194f;
+		
+		System.out.println("Coordinates of a location (latitude, longitude):"+latitude+","+longitude);
+		
+		
+		
+		// Person's marital status: true or false
+		
+		boolean marital_status=true;
+		System.out.println("Person's marital status:"+marital_status);
+		
+		
+		// Person's occupation: "Software Engineer"
+		String occupation="Software Engineer";
+		System.out.println("Person's occupation:"+occupation);
+		
+		
+		
+		// Person's favourite colour: "Blue"
+		String colour="blue";
+		System.out.println("Person's favourite colour:"+colour);
+		
+		// Current year: 2023
+		int year=2023;
+		System.out.println("current year:"+year);
+		
+			
+		
+		// Number of followers on a social media platform: 1,000,000
+		long followers_count=1000000l;
+		
+		System.out.println("Number of followers on a social media platform:"+followers_count);
+		
+		// Rating of a movie: 7.5
+		float rating=7.5f;
+		System.out.println("Rating of a movie:"+rating);
+		
+		// Person's blood type: 'A'
+		char blood_type='A';
+		System.out.println("Person's blood type:"+blood_type);
+		
+		// Title of a book: "To Kill a Mockingbird"
+		
+		String book_title="To Kill a Mockingbird";
+		System.out.println("Title of a book:"+book_title);
+		
+		// Number of employees in a company: 500
+		int employees=500;
+		System.out.println("Number of employees in a company:"+ employees);
+		
+		
+		// Time of an event: 2:30 PM
+		String time_event="2:30PM";
+		System.out.println("Time of an event:"+time_event);
+		
+		// Name of a country: "United States"
+		String country="United States";
+		System.out.println("Name of a country: "+ country);
+		
+		// Person's eye color: "Brown"
+		String Color="Brown";
+		System.out.println("Person's eye color: "+ Color);
+		
+		// Person's birthplace: "New York City"
+		String birth_place="New York City";
+		System.out.println("Person's birthplace:"+ birth_place);
+		
+		// Distance between two cities: 200.5
+		float distance=200.5f;
+		System.out.println("Distance between two cities: "+ distance);
+
+		
+		
+	}
+
+}
