889f0d9666c1d13143eb1f246824f353c54b57bf
Naveen_Prasath <naveenmailid95@gmail.com>
2025-08-24 23:19:19 +0530

    assginment11half

---
+++ src/assignments/Assignment11.java
@@ -0,0 +1,33 @@
+package assignments;
+
+public class Assignment11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int Arr[]=new int [] {1,2,3,4,5};
+		
+		//System.out.printf("%10d",a);
+		int j=5;
+		
+		for (int i=0;i<Arr.length;i++)
+		
+			
+		{
+			
+			for (int k=0;k<=(Arr.length)-j;k++)
+			{
+				System.out.print(Arr[k]);
+				
+				
+			}
+			System.out.printf("%n");
+			j--;
+			
+			}
+			
+		}
+	
+
+}
+
